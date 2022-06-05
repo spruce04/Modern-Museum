0aae8846dd83b5f5043e2eea40814310315d7cf3
Sam Flax <sam.flax2005@gmail.com>
2022-06-05 17:53:35 +1000

    add previous and next buttons

--- script.js
+++ script.js
@@ -1,65 +1,11 @@
 //dom selectors
 const slider = document.getElementById("slideMe");
 const content = document.getElementById("content");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
 
-//determine the year based on slider range
-function year(param){
-  switch (param) {
-    case ('1'):
-      return '1892';
-    case ('2'):
-      return '1907';
-    case ('3'):
-      return '1912';
-    case ('4'):
-      return '1913'
-    case ('5'):
-      return '1915';
-    case ('6'):
-      return '1923';
-    case ('7'):
-      return '1926';
-    case ('8'):
-      return '1931';
-    case ('9'):
-      return '1933';
-    case ('10'):
-      return '1936';
-    case ('11'):
-      return '1937';
-    case ('12'):
-      return '1938';
-    case ('13'):
-      return '1939';
-    case ('14'):
-      return '1940';
-    case ('15'):
-      return '1943';
-    case ('16'):
-      return '1946';
-    case ('17'):
-      return '1947';
-    case ('18'):
-      return '1948';
-    case ('19'):
-      return '1952';
-    case ('20'):
-      return '1966';
-    case ('21'):
-      return '1967';
-    case ('22'):
-      return '1969';
-    case ('23'):
-      return '1973';
-    case ('24'):
-      return '1975';
-    case ('25'):
-      return '1977';
-}};
-
-
-
-slider.addEventListener("click", function() {
+//determine text based on slider value
+function textDecide() {
   let yearNum = year(slider.value);
   switch (slider.value) {
     case ('1'):
@@ -137,84 +83,102 @@ slider.addEventListener("click", function() {
     case ('25'):
       content.innerHTML = `${yearNum}: Spain completed its transition to a democratic government. The era of Franco's regime had ended.`;
       break;
-}}); 
+}}; 
 
-slider.addEventListener("touchend", function() {
-  let yearNum = year(slider.value);
-  switch (slider.value) {
+//function to move backwards
+function movePrevious() {
+  if (slider.value == 1) {
+    return; 
+  }
+  else {
+    slider.value = slider.value - 1;
+    textDecide();
+  }
+};
+
+//function to move forwards
+function moveNext() {
+  if (slider.value == 25) {
+    return;
+  }
+  else {
+    slider.value = parseInt(slider.value) + 1;
+    textDecide();
+  }
+};
+
+//determine the year based on slider range
+function year(param){
+  switch (param) {
     case ('1'):
-      content.innerHTML = `${yearNum}: Francisco Franco was born in El Ferrol, Spain to a middle class family`;
-      break;
+      return '1892';
     case ('2'):
-      content.innerHTML = `${yearNum}: At only 14 years old, Franco enrolled in the Infantry academy in Toledo.`;
-      break;
+      return '1907';
     case ('3'):
-      content.innerHTML = `${yearNum}: Franco volunteered for active duty in Spanish Morocco.`
-      break;
+      return '1912';
     case ('4'):
-      content.innerHTML = `${yearNum}: Franco was promoted to first lieutenant. Franco’s quick climb through the ranks of the Spanish military can be attributed to professionalism and attention to the wellbeing of his troops.`;
-      break;
+      return '1913'
     case ('5'):
-      content.innerHTML = `${yearNum}: Franco became the youngest captain in the Spanish military.`
-      break;
+      return '1915';
     case ('6'):
-      content.innerHTML = `${yearNum}: Franco married his wife, Carmen Polo.`;
-      break;
+      return '1923';
     case ('7'):
-      content.innerHTML = `${yearNum}: By this time, Franco had been involved in many military campaigns and became a national hero. In 1926 he was promoted to brigadier general and continued his leading role in the Spanish military.`;
-      break;
+      return '1926';
     case ('8'):
-      content.innerHTML = `${yearNum}: The Spanish monarchy fell after socialists and republicans won elections across Spain. Military reform was enacted in Spain, and Franco was made inactive. Despite being a monarchist, Franco accepted this without protest.`;
-      break;
+      return '1931';
     case ('9'):
-      content.innerHTML = `${yearNum}: A far right party backed by Italy won the Spanish general elections. Unrest and political tensions spiralled in Spain.`;
-      break;
+      return '1933';
     case ('10'):
-      content.innerHTML = `${yearNum}: The Popular Front, a coalition of left-wing parties, won the Spanish election. Anarchy grew rapidly. Franco appealed to the government to enforce a state of emergency. They refused, and he was sent to the Canary Islands. Other right-wing military leaders began plotting rebellion. The far-right leader of this rebellion was assassinated. After seeing the growing anarchy in Spain, Franco eventually joined, and the Spanish Civil War began in North Africa. General Franco was formally given the title of Caudillo as the leader of the Nationalists (rebels).`;
-      break;
+      return '1936';
     case ('11'):
-      content.innerHTML = `${yearNum}: The civil war raged throughout Spain, with the USSR and France allying with the Republicans, and Germany and Italy allying with Franco's nationalists.`;
-      break;
+      return '1937';
     case ('12'):
-      content.innerHTML = `${yearNum}: The battle of the Ebro, the longest and largest battle of the war, resulted in victopry for Franco. Spanish media was put under censorship.`;
-      break;
+      return '1938';
     case ('13'):
-      content.innerHTML = `${yearNum}: Barcelona and Madrid were captured by Nationalist forces. The civil war ended and the Falange became the sole party of Spain, with Franco as leader. World War 2 began.`;
-      break;
+      return '1939';
     case ('14'):
-      content.innerHTML = `${yearNum}: After the fall of France, Franco indicated willingness to bring Spain into the war on Germany’s side, however, an agreement was never reached.`;
-      break;
+      return '1940';
     case ('15'):
-      content.innerHTML = `${yearNum}: Spain returned to complete neutrality, however, it was too late in the war to gain any favours from the allied victors.`;
-      break;
+      return '1943';
     case ('16'):
-      content.innerHTML = `${yearNum}: The UN security council denied Franco’s regime recognition until it developed a representative government.`;
-      break;
+      return '1946';
     case ('17'):
-      content.innerHTML = `${yearNum}: A referendum made Spain a monarchy, and Franco sovereign for life, with the sole responsibility of appointing a successor.`;
-      break;
+      return '1947';
     case ('18'):
-      content.innerHTML = `${yearNum}: Franco began forming stronger relations with other countries.`;
-      break;
+      return '1948';
     case ('19'):
-      content.innerHTML = `${yearNum}: Spain signed a 10 year military assistance pact with the United States.`;
-      break;
+      return '1952';
     case ('20'):
-      content.innerHTML = `${yearNum}: A referendum was held which approved a new constitution, limiting and defining Franco’s powers.`;
-      break;
+      return '1966';
     case ('21'):
-      content.innerHTML = `${yearNum}: Franco opened elections for a small minority of deputies to parliament.`;
-      break;
+      return '1967';
     case ('22'):
-      content.innerHTML = `${yearNum}: Franco assigned 32 year old Juan Carlos (the grandson of the last Spanish king) as the next to take the Spanish throne. `;
-      break;
+      return '1969';
     case ('23'):
-      content.innerHTML = `${yearNum}: Franco resigned as ‘premier’ but still retained his titles as head of state, commander of armed forces, and head of the ‘movement’.`;
-      break;
+      return '1973';
     case ('24'):
-      content.innerHTML = `${yearNum}: Death of Francisco Franco. Juan Carlos takes the throne.`;
-      break;
+      return '1975';
     case ('25'):
-      content.innerHTML = `${yearNum}: Spain completed its transition to a democratic government. The era of Franco's regime had ended.`;
-      break;
-}}); 
+      return '1977';
+}};
+
+//change text when slider is touched - computer
+slider.addEventListener("click", function() {
+  textDecide();
+});
+//change text when slider is touched - mobile
+slider.addEventListener("touchend", function() {
+  textDecide()
+}); 
+
+//move to previous when previous button is pressed
+previous.addEventListener("click", function() {
+  movePrevious();
+});
+//move to next when next button is clicked
+next.addEventListener("click", function() {
+  moveNext();
+})
+
+//set text to beginning when program initially loads
+textDecide();
