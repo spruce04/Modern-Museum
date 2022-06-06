9a15d8585f64a1c1b9d1825cc38612c92afb941f
spruce04 <sam.flax2005@gmail.com>
2022-06-06 10:22:38 +1000

    added year display

--- script.js
+++ script.js
@@ -3,85 +3,86 @@ const slider = document.getElementById("slideMe");
 const content = document.getElementById("content");
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
+const yearDisplay = document.getElementById("yearText");
 
 //determine text based on slider value
 function textDecide() {
-  let yearNum = year(slider.value);
+  yearDisplay.innerHTML = year(slider.value);
   switch (slider.value) {
     case ('1'):
-      content.innerHTML = `${yearNum}: Francisco Franco was born in El Ferrol, Spain to a middle class family`;
+      content.innerHTML = `Francisco Franco was born in El Ferrol, Spain to a middle class family`;
       break;
     case ('2'):
-      content.innerHTML = `${yearNum}: At only 14 years old, Franco enrolled in the Infantry academy in Toledo.`;
+      content.innerHTML = `At only 14 years old, Franco enrolled in the Infantry academy in Toledo.`;
       break;
     case ('3'):
-      content.innerHTML = `${yearNum}: Franco volunteered for active duty in Spanish Morocco.`
+      content.innerHTML = `Franco volunteered for active duty in Spanish Morocco.`
       break;
     case ('4'):
-      content.innerHTML = `${yearNum}: Franco was promoted to first lieutenant. Franco’s quick climb through the ranks of the Spanish military can be attributed to professionalism and attention to the wellbeing of his troops.`;
+      content.innerHTML = `Franco was promoted to first lieutenant. Franco’s quick climb through the ranks of the Spanish military can be attributed to professionalism and attention to the wellbeing of his troops.`;
       break;
     case ('5'):
-      content.innerHTML = `${yearNum}: Franco became the youngest captain in the Spanish military.`
+      content.innerHTML = `Franco became the youngest captain in the Spanish military.`
       break;
     case ('6'):
-      content.innerHTML = `${yearNum}: Franco married his wife, Carmen Polo.`;
+      content.innerHTML = `Franco married his wife, Carmen Polo.`;
       break;
     case ('7'):
-      content.innerHTML = `${yearNum}: By this time, Franco had been involved in many military campaigns and became a national hero. In 1926 he was promoted to brigadier general and continued his leading role in the Spanish military.`;
+      content.innerHTML = `By this time, Franco had been involved in many military campaigns and became a national hero. In 1926 he was promoted to brigadier general and continued his leading role in the Spanish military.`;
       break;
     case ('8'):
-      content.innerHTML = `${yearNum}: The Spanish monarchy fell after socialists and republicans won elections across Spain. Military reform was enacted in Spain, and Franco was made inactive. Despite being a monarchist, Franco accepted this without protest.`;
+      content.innerHTML = `The Spanish monarchy fell after socialists and republicans won elections across Spain. Military reform was enacted in Spain, and Franco was made inactive. Despite being a monarchist, Franco accepted this without protest.`;
       break;
     case ('9'):
-      content.innerHTML = `${yearNum}: A far right party backed by Italy won the Spanish general elections. Unrest and political tensions spiralled in Spain.`;
+      content.innerHTML = `A far right party backed by Italy won the Spanish general elections. Unrest and political tensions spiralled in Spain.`;
       break;
     case ('10'):
-      content.innerHTML = `${yearNum}: The Popular Front, a coalition of left-wing parties, won the majority of seats in the Spanish election. Anarchy grew rapidly, and The Popular Front was unable to control it. Franco had never aligned himself with a political party, and appealed to the government to enforce a state of emergency. They refused, and he was sent to the Canary Islands. Right-wing military leaders began plotting rebellion, and the leader was assassinated. Franco initially resisted joining, but seeing the growing anarchy in Spain, he eventually gives in, and the Spanish Civil War beganin North Africa. Various world powers, such as Portugal, Germany, Italy and USSR made “non-intervention” agreements however the agreements were not followed. General Franco was formally given the title of Caudillo as the leader of the Nationalists (rebels).`;
+      content.innerHTML = `The Popular Front, a coalition of left-wing parties, won the majority of seats in the Spanish election. Anarchy grew rapidly, and The Popular Front was unable to control it. Franco had never aligned himself with a political party, and appealed to the government to enforce a state of emergency. They refused, and he was sent to the Canary Islands. Right-wing military leaders began plotting rebellion, and the leader was assassinated. Franco initially resisted joining, but seeing the growing anarchy in Spain, he eventually gives in, and the Spanish Civil War beganin North Africa. Various world powers, such as Portugal, Germany, Italy and USSR made “non-intervention” agreements however the agreements were not followed. General Franco was formally given the title of Caudillo as the leader of the Nationalists (rebels).`;
       break;
     case ('11'):
-      content.innerHTML = `${yearNum}: The civil war raged throughout Spain, with the USSR and France allying with the Republicans, and Germany and Italy allying with Franco's nationalists.`;
+      content.innerHTML = `The civil war raged throughout Spain, with the USSR and France allying with the Republicans, and Germany and Italy allying with Franco's nationalists.`;
       break;
     case ('12'):
-      content.innerHTML = `${yearNum}: The battle of the Ebro, the longest and largest battle of the war, resulted in victopry for Franco. Spanish media was put under censorship.`;
+      content.innerHTML = `The battle of the Ebro, the longest and largest battle of the war, resulted in victopry for Franco. Spanish media was put under censorship.`;
       break;
     case ('13'):
-      content.innerHTML = `${yearNum}: Barcelona and Madrid were captured by Nationalist forces. The civil war ended and the Falange became the sole party of Spain, with Franco as leader. World War 2 began.`;
+      content.innerHTML = `Barcelona and Madrid were captured by Nationalist forces. The civil war ended and the Falange became the sole party of Spain, with Franco as leader. World War 2 began.`;
       break;
     case ('14'):
-      content.innerHTML = `${yearNum}: After the fall of France, Franco indicated willingness to bring Spain into the war on Germany’s side, however, an agreement was never reached.`;
+      content.innerHTML = `After the fall of France, Franco indicated willingness to bring Spain into the war on Germany’s side, however, an agreement was never reached.`;
       break;
     case ('15'):
-      content.innerHTML = `${yearNum}: Spain returned to complete neutrality, however, it was too late in the war to gain any favours from the allied victors.`;
+      content.innerHTML = `Spain returned to complete neutrality, however, it was too late in the war to gain any favours from the allied victors.`;
       break;
     case ('16'):
-      content.innerHTML = `${yearNum}: The UN security council denied Franco’s regime recognition until it developed a representative government.`;
+      content.innerHTML = `The UN security council denied Franco’s regime recognition until it developed a representative government.`;
       break;
     case ('17'):
-      content.innerHTML = `${yearNum}: A referendum made Spain a monarchy, and Franco sovereign for life, with the sole responsibility of appointing a successor.`;
+      content.innerHTML = `A referendum made Spain a monarchy, and Franco sovereign for life, with the sole responsibility of appointing a successor.`;
       break;
     case ('18'):
-      content.innerHTML = `${yearNum}: Franco began forming stronger relations with other countries.`;
+      content.innerHTML = `Franco began forming stronger relations with other countries.`;
       break;
     case ('19'):
-      content.innerHTML = `${yearNum}: Spain signed a 10 year military assistance pact with the United States.`;
+      content.innerHTML = `Spain signed a 10 year military assistance pact with the United States.`;
       break;
     case ('20'):
-      content.innerHTML = `${yearNum}: A referendum was held which approved a new constitution, limiting and defining Franco’s powers.`;
+      content.innerHTML = `A referendum was held which approved a new constitution, limiting and defining Franco’s powers.`;
       break;
     case ('21'):
-      content.innerHTML = `${yearNum}: Franco opened elections for a small minority of deputies to parliament.`;
+      content.innerHTML = `Franco opened elections for a small minority of deputies to parliament.`;
       break;
     case ('22'):
-      content.innerHTML = `${yearNum}: Franco assigned 32 year old Juan Carlos (the grandson of the last Spanish king) as the next to take the Spanish throne. `;
+      content.innerHTML = `Franco assigned 32 year old Juan Carlos (the grandson of the last Spanish king) as the next to take the Spanish throne. `;
       break;
     case ('23'):
-      content.innerHTML = `${yearNum}: Franco resigned as ‘premier’ but still retained his titles as head of state, commander of armed forces, and head of the ‘movement’.`;
+      content.innerHTML = `Franco resigned as ‘premier’ but still retained his titles as head of state, commander of armed forces, and head of the ‘movement’.`;
       break;
     case ('24'):
-      content.innerHTML = `${yearNum}: Death of Francisco Franco. Juan Carlos takes the throne.`;
+      content.innerHTML = `Death of Francisco Franco. Juan Carlos takes the throne.`;
       break;
     case ('25'):
-      content.innerHTML = `${yearNum}: Spain completed its transition to a democratic government. The era of Franco's regime had ended.`;
+      content.innerHTML = `Spain completed its transition to a democratic government. The era of Franco's regime had ended.`;
       break;
 }}; 
 
